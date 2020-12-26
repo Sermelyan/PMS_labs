35dcd7ed53ffa4002bc2941c8a2fa0328fc2300b
Sergey Melikyan <melikyan.sa@ya.ru>
2020-12-26 19:08:17 +0300

    finished dz1

--- dz1_var19/main.s
+++ dz1_var19/main.s
@@ -10,44 +10,56 @@
 Reset_Handler PROC
 	export Reset_Handler
 	ldr R0, =arr1
-	ldr R1, =5
+	ldr R1, =9
 	bl	crc8
 	
 	ldr R0, =arr2
-	ldr R1, =6
+	ldr R1, =5
 	bl	crc8
+	
+	ldr R0, =arr3
+	ldr R1, =8
+	bl	crc8
+	
+	b loop
+	
+	ENDP
+		
+loop PROC
+	b loop
 	
 	ENDP
 
 crc8 PROC
-	ldr R2, =0xff
+	ldr R2, =0x0
 	
 outter
 	ldrb R3, [R0], #1
 	eor R2, R2, R3
-	ldr R4, =8
+	ldr R3, =8
 	
 inner
-	lsl R6, R2, #1
+	lsl R4, R2, #1
 	ands R5, R2, #0x80
 	ite ne
-	eorne R2, R6, #0x31
-	moveq R2, R6
+	eorne R2, R4, #0xd5
+	moveq R2, R4
 	
-	subs R4, #1
+	subs R3, #1
 	bne inner
 	
 	subs R1, #1
 	bne outter
 	
+	bfc R2, #8, #24
 	bx LR
 	
 	ENDP
 
 	AREA SRC, DATA, READONLY
 	
-arr1	DCB 1,2,3,4,5,6,7,8,9
+arr1	DCB "123456789"
 arr2	DCB "hello"
-arr3	DCB 0,12,145,255,13,1
+arr3	DCB "Melikyan"
 		
 	END
